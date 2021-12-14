fc6dcd4589f2bd5c18a9b6886554de435b249aed
Jonathas <jonathaseloi@gmail.com>
2021-12-14 19:11:32 -0300

    Refactor

--- app/services/notify_partner.rb
+++ app/services/notify_partner.rb
@@ -4,5 +4,8 @@ class NotifyPartner
     @message = message
   end
 
-  def perform; end
+  def perform
+    url = "https://61b69749c95dd70017d40f4b.mockapi.io/awesome_partner_leads"
+    Faraday.post(url, { partner: @partner, message: @message })
+  end
 end
